c6ca93e54cf923d81f51f3f4eb0fe8b27be730d4
xys20071111 <xys20071111@hotmail.com>
2021-08-14 20:35:45 +0800

    将json生成改成函数

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+from asyncio.unix_events import AbstractChildWatcher
 from websockets import client
 from bilibili_api.live import LiveDanmaku
 import json
 import asyncio
+import sys
 
 bot_data = {
     'session': ''
 }
+
+
+def gen_message(target: int, text: str):
+    data_body = {
+        'sessionKey': bot_data['session'],
+        'target': target,
+        'messageChain': [{'type': 'Plain', 'text': text}]
+    }
+    msg_body = {
+        'syncId': 'sendGroupMessage',
+        'command': 'sendGroupMessage',
+        'subCommand': None,
+        'content': data_body
+    }
+    return json.dumps(msg_body)
 
 
 async def main():
@@ -20,43 +37,31 @@ async def main():
     config_file.close()
     # 登陆 mriai 接口
     ws = await client.connect(API_URL+f'/message?verifyKey={AUTH}&qq={QQ}')
+    session_info = json.loads(await ws.recv())['data']
+    if session_info['code'] == 0:
+        print('Auth succeed')
+        bot_data['session'] = session_info['session']
+        for v in GROUPS:
+            pass
+            # await ws.send(gen_message(v,'机器人上线'))
+    else:
+        print('Auth failed')
+        sys.exit(0)
     live_danmaku = LiveDanmaku(config['room_id'], max_retry=5000)
 
     @live_danmaku.on('LIVE')
     async def live(arg):
         for v in GROUPS:
-            data_body = {
-                'sessionKey': bot_data['session'],
-                'target': v,
-                'messageChain': [{'type': 'Plain', 'text': TEXT}]
-            }
-            msg_body = {
-                'syncId': 'sendGroupMessage',
-                'command': 'sendGroupMessage',
-                'subCommand': None,
-                'content': data_body
-            }
-        await ws.send(json.dumps(msg_body))
+            await ws.send(gen_message(v, TEXT))
 
     danmaku = asyncio.create_task(live_danmaku.connect())
     # 用来检查bot是否运行的命令
 
     async def group_message_handler():
         while True:
-            messages = json.loads(await ws.recv())
-            if messages['type'] == 'GroupMessage' and messages['sender']['group']['id'] in GROUPS and messages['messageChain'][1]['text'] == '/botStatus':
-                data_body = {
-                    'sessionKey': bot_data['session'],
-                    'target': messages['sender']['group']['id'],
-                    'messageChain': [{'type': 'Plain', 'text': TEXT}]
-                }
-                msg_body = {
-                    'syncId': 'sendGroupMessage',
-                    'command': 'sendGroupMessage',
-                    'subCommand': None,
-                    'content': data_body
-                }
-                await ws.send(json.dumps(msg_body))
+            messages = json.loads(await ws.recv())['data']
+            if messages['type'] == 'GroupMessage' and messages['sender']['group']['id'] in GROUPS and messages['messageChain'][1]['type'] == 'Plain' and messages['messageChain'][1]['text'] == '/botStatus':
+                await ws.send(gen_message(messages['sender']['group']['id'], '开播通知机器人运行中'))
             await asyncio.sleep(0.5)
 
     msg_task = asyncio.create_task(group_message_handler())
